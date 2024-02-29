e66592b9f7a5f7ddd000f342cd71e3d9a0b9c79d
shaikha <jshaikha0@gmail.com>
2024-02-29 18:42:41 +0300

    sign up css

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -3,6 +3,7 @@ import Client from "./api"
 export const RegisterUser = async (data) => {
   try {
     const response = await Client.post("/auth/register", data)
+    console.log(response.data)
     return response.data
   } catch (error) {
     throw error
