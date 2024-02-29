8278dfa74893cfbe66385d61a816631bd73341a3
shaikha <jshaikha0@gmail.com>
2024-02-29 20:27:32 +0300

    installing react-router-dom importing  BrowserRouter to the main - creating Nav component - using useNavigate and working on login page + css

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+ 
+const Nav = () => {
+  
+  return (
+  <div>
+      <Link to="/login">login</Link>
+      <Link to="/register">Register</Link>
+    
+    </div>
+  )
+}
+
+export default Nav
