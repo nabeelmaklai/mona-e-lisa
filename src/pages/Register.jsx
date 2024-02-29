6e423d51008dbd415b124a0184c1afcc261147fd
shaikha <jshaikha0@gmail.com>
2024-02-29 14:28:49 +0300

    creating a BASE_URL

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import axios from "axios"
 const Register = () => {
   const [formValues, setFormValues] = useState({
     name: "",
@@ -7,6 +7,23 @@ const Register = () => {
     password: "",
     confirmPassword: "",
   })
+  const handleChange = (event) => {
+    setFormValues({ ...formValues, [event.target.name]: event.target.value })
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    await RegisterUser({
+      name: formValues.name,
+      email: formValues.email,
+      password: formValues.password,
+    })
+    setFormValues({
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    })
+  }
   return (
     <div>
       <h1></h1>
