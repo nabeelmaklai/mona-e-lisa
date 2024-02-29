66d98db656207b0ff37ac916d10ee277251e36b3
shaikha <jshaikha0@gmail.com>
2024-02-29 14:16:17 +0300

    using useState for Register form and installing axios

---
+++ src/pages/Register.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+const Register = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  })
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  )
+}
+export default Register
