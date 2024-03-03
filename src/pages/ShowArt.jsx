53fa2b07202f168fc11a2199fd7e3c53868e5d9a
9Jassim <jassim9.a@gmail.com>
2024-03-03 12:29:37 +0300

    show comments on art page

--- src/pages/ShowArt.jsx
+++ src/pages/ShowArt.jsx
@@ -5,35 +5,55 @@ import Client from '../services/api'
 const ShowArt = ({ user }) => {
   let { id } = useParams()
   const [art, setArt] = useState(null)
-  const commentRef = {
+  const [commented, setCommented] = useState(false)
+  let commentRef = {
     body: useRef(null),
     artId: id,
-    userId: user.id
+    userId: useRef(null)
   }
 
   useEffect(() => {
     const getArt = async () => {
       const response = await showArt(id)
       setArt(response)
+      setCommented(false)
     }
     getArt()
-  }, [])
+  }, [commented])
 
   const addComment = async () => {
-    await Client.post(`/arts/${id}/comments`, commentRef)
+    const comment = {
+      body: commentRef.body.current.value,
+      artId: id,
+      userId: commentRef.userId.current.value
+    }
+    await Client.post(`/arts/${id}/comments`, comment)
+    setCommented(true)
+
+    commentRef.body.current.value = ''
+    commentRef.userId.current.value = ''
   }
 
   return art ? (
     <div className="show-art">
-      {/* <h5>{art.userId.name}</h5> */}
+      <h5>{art.userId.name}</h5>
       <h4>{art.name}</h4>
       <img src={art.img} alt="{art.userId.name}" />
       <p>{art.description}</p>
       <div className="comments-section">
         <input type="text" ref={commentRef.body} />
+        <input
+          type="hidden"
+          hidden
+          ref={commentRef.userId}
+          value={art.userId._id}
+        />
         <button onClick={addComment}>Comment</button>
         {art.commentIds.map((comment) => (
-          <div>{comment.body}</div>
+          <div key={comment._id}>
+            {comment.userId.name}
+            {comment.body}
+          </div>
         ))}
       </div>
     </div>
