2c22a9f628ac76bc7a31c14c8ee2cdaf61194e88
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-03-05 16:11:18 +0300

    view collection or view art on profile page functionality

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom'
 import AddArt from '../components/AddArt'
 import FollowButton from '../components/FollowButton'
 import { Avatar } from '@mui/material'
+import ShowCollection from './ShowCollection'
 
 const User = ({ user }) => {
   let { id } = useParams()
@@ -13,7 +14,9 @@ const User = ({ user }) => {
   const [addArtForm, setaddArtForm] = useState(false)
   const [following, setFollowing] = useState(false)
   const [profile, setProfile] = useState({})
-  const [collection, setCollection] = useState([])
+  const [collections, setCollections] = useState([])
+  const [showCollection, setShowCollection] = useState(false)
+  const [showArts, setShowArts] = useState(true)
 
   const [newArt, setNewArt] = useState({
     name: '',
@@ -31,12 +34,16 @@ const User = ({ user }) => {
       const newFollowingList = response1?.following.map((user) => user._id)
       newFollowingList.includes(id) ? setFollowing(true) : setFollowing(false)
       setArt(response.artIds)
-      console.log('this is the response', response.collectionIds)
-      setCollection(response.collectionIds)
+      console.log(
+        'this is the response from the get user content',
+        response.collectionIds
+      )
+      setCollections(response.collectionIds)
       setProfile(response)
     }
+
     getUserContent()
-  }, [id, following])
+  }, [id, following, showCollection])
 
   const hadleChange = (event) => {
     setNewArt({ ...newArt, [event.target.name]: event.target.value })
@@ -56,15 +63,54 @@ const User = ({ user }) => {
   const handleAddArtButton = () => {
     addArtForm ? setaddArtForm(false) : setaddArtForm(true)
   }
+  const handlecollectionButton = () => {
+
+    setShowCollection(true)
+    setShowArts(false)
+  }
+  const handleArtButton = () => {
+
+    setShowArts(true)
+    setShowCollection(false)
+  }
 
   const navigateToAddCollection = () => {
     navigate('/collections')
   }
+  {
+    addArtForm && (
+      <AddArt handleAddArt={handleAddArt} hadleChange={hadleChange} />
+    )
+  }
 
   return (
     <div>
+      {user ? (
+        user.id === id && (
+          <button onClick={handlecollectionButton}>Collections</button>
+        )
+      ) : (
+        <></>
+      )}
 
-      <div className="post__headerAuthor"><Avatar/><h2 className='h2'>{profile.name}</h2></div>
+      {user ? (
+        user.id === id && <button onClick={handleArtButton}>Art</button>
+      ) : (
+        <></>
+      )}
+
+      {showCollection &&
+        collections.map((collection) => (
+          <Link to={`/collections/${collection._id}`} key={collection._id}>
+            <img src={collection.artIds[0].img} alt={collection.name} />
+            <p>Name:{collection.name}</p>
+          </Link>
+        ))}
+
+      <div className="post__headerAuthor">
+        <Avatar />
+        <h2 className="h2">{profile.name}</h2>
+      </div>
 
       <div>Email: {profile.email}</div>
       {user ? (
@@ -79,21 +125,21 @@ const User = ({ user }) => {
       ) : (
         <></>
       )}
-      <div><div className='profileImgDiv'>
-        {art &&
-          art.map((piece) => (
-            <Link key={piece._id} to={`/arts/${piece._id}`}>
-
-              <img className="HomeArtImgs ProfileImgs" src={piece.img} alt={piece.name} key={piece._id} />
-            </Link>
-          ))}</div>
-        {art &&
-          collection.map((piece) => (
-            <Link to={`/collections/${piece._id}`}>
-              {/* <img src={piece.img} alt={piece.name} key={piece._id} /> */}
-              <p>{piece.name}Name:</p>
-            </Link>
-          ))}
+      <div>
+        <div className="profileImgDiv">
+          {art &&
+            showArts &&
+            art.map((piece) => (
+              <Link key={piece._id} to={`/arts/${piece._id}`}>
+                <img
+                  className="HomeArtImgs ProfileImgs"
+                  src={piece.img}
+                  alt={piece.name}
+                  key={piece._id}
+                />
+              </Link>
+            ))}
+        </div>
 
         {user ? (
           user.id === id && (
