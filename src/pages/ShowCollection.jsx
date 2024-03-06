0f4510f6d265cbd436bd743788c215c131f14c9b
9Jassim <jassim9.a@gmail.com>
2024-03-06 10:51:33 +0300

    remove art from collection

--- src/pages/ShowCollection.jsx
+++ src/pages/ShowCollection.jsx
@@ -1,11 +1,14 @@
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import { showCollection } from '../services/Get'
+import Client from '../services/api'
+import { Link } from 'react-router-dom'
 
-const ShowCollection = () => {
+const ShowCollection = ({ user }) => {
   let { id } = useParams()
   const [collection, setCollection] = useState({})
   const [collectionArt, setCollectionArt] = useState([])
+  const [removed, setRemoved] = useState(false)
 
   useEffect(() => {
     const getCollection = async () => {
@@ -15,16 +18,34 @@ const ShowCollection = () => {
       console.log('this is the response from show collection', response.artIds)
     }
     getCollection()
-  }, [])
+    setRemoved(false)
+  }, [removed])
+
+  const remove = async (artId) => {
+    Client.put(`/collections/${id}/remove`, { artId })
+    setRemoved(true)
+  }
 
   return (
     <div className="show-collection">
-      
-      <h4>{collection.name}[{collection.description}]</h4>
+      <h4>
+        {collection.name}[{collection.description}]
+      </h4>
       <p></p>
 
       {collectionArt.map((art) => (
-        <img className='ShowArtImg  img-resize resize'  src={art.img} alt={art.name} />
+        <div>
+          <Link to={`/arts/${art._id}`}>
+            <img
+              className="ShowArtImg  img-resize resize"
+              src={art.img}
+              alt={art.name}
+            />
+          </Link>
+          {collection.userId === user.id && (
+            <button onClick={() => remove(art._id)}>Remove</button>
+          )}
+        </div>
       ))}
     </div>
   )
